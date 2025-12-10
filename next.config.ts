398d0d54dd1c64215ebd9b52d11e4fce79d51933
jesusalbarca <100383415@alumnos.uc3m.es>
2025-12-10 21:01:59 +0100

    config gh pages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/download',
+  assetPrefix: '/download/',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
